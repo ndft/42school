1f99b077c6bdcb3eae1ffa41871db73a98a18cc6
Andrey Udalov <mgp495@gmail.com>
2021-11-13 00:13:12 +0300

    new itoa

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -1,30 +1,47 @@
 #include "libft.h"
 
-char	*ft_itoa(int n)
+static int  str_len(int n)
 {
-	char	*str;
-	long	nbr;
-	size_t	size;
+    int len;
 
-	nbr = n;
-	size = n > 0 ? 0 : 1;
-	nbr = nbr > 0 ? nbr : -nbr;
-	while (n)
-	{
-		n /= 10;
-		size++;
-	}
-	if (!(str = (char *)malloc(size + 1)))
-		return (0);
-	*(str + size--) = '\0';
-	while (nbr > 0)
-	{
-		*(str + size--) = nbr % 10 + '0';
-		nbr /= 10;
-	}
-	if (size == 0 && str[1] == '\0')
-		*(str + size) = '0';
-	else if (size == 0 && str[1] != '\0')
-		*(str + size) = '-';
-	return (str);
+    len = 0;
+    if (n < 0)
+        len++;
+    if (n == 0)
+        len = 1;
+    while (n)
+    {
+        len++;
+        n = n / 10;
+    }
+    return (len);
+}
+
+char    *ft_itoa(int n)
+{
+    char    *str;
+    int     len;
+
+    len = str_len(n);
+    str = ft_strnew(len);
+    if (!str)
+        return (NULL);
+    if (n == 0)
+        str[0] = '0';
+    if (n < 0)
+    {
+        str[0] = '-';
+        if (n == -2147483648)
+        {
+            str[len-- - 1] = '8';
+            n = n / 10;
+        }
+        n = -n;
+    }
+    while (n != 0 && len >= 0)
+    {
+        str[len-- - 1] = n % 10 + 48;
+        n = n / 10;
+    }
+    return (str);
 }
